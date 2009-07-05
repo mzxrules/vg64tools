b62d3c3296a41371b004163f89561886906fa6c5
spinout_182@yahoo.com <spinout_182@yahoo.com@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-05 10:25:10 +0000

    small changes that go along with datatypes that actors read/write for their actor entries. Also, added a struct of a "actor info header" or whatever you want to call it.

--- n64/z_snippets/actor.h
+++ n64/z_snippets/actor.h
@@ -3,12 +3,26 @@
 
 #include "mips.h"
 
+struct actor_spec_t
+{
+    u16 actor_number;
+    u8 actor_type;
+    u8 actor_status;
+    u32 _NULL;
+    u16 object_number;
+    u16 _NULL_;
+    u32 _NULL__;
+    u32 init;
+    u32 func_1;
+    u32 func_2;
+    u32 func_3;
+};
 
 struct z64_actor_t
 {
     u16 actor_number;           /* actor number                         */
     u8  actor_type;             /* 2 = link 4 = npcs etc.               */
-    u8  unknown_1;              /* unknown (Z-Target related, perhaps?) */
+    u8  actor_status;           /* unknown (Z-Target related, perhaps?) */
     u32 unknown_;
     f32 x_unknown;              /* Does not affect actual x position, but is constantly written to */
     f32 y_unknown;              /* Same, but for Y                      */
@@ -55,50 +69,64 @@ struct z64_actor_t
     
     int _0x0068;
     int _0x006C;
+    /* 0x70 */
     int _0x0070;
     int _0x0074;
     int _0x0078;
     int _0x007C;
+    /* 0x80 */
     int _0x0080;
     int _0x0084;
     int _0x0088;
     int _0x008C;
+    /* 0x90 */
     int _0x0090;
     int _0x0094;
     int _0x0098;
     int _0x009C;
+    /* 0xA0 */
     int _0x00A0;
     int _0x00A4;
     int _0x00A8;
-    int _0x00AC;
+    u16 _0x00AC;
+    u8  _0x00AE;
+    u8  _0x00AF;
+    /* 0xB0 */
     int _0x00B0;
     int _0x00B4;
     int _0x00B8;
-    int _0x00BC;
+    f32 _0x00BC;
+    /* 0xC0 */
     int _0x00C0;
     int _0x00C4;
     int _0x00C8;
     int _0x00CC;
+    /* 0xD0 */
     int _0x00D0;
     int _0x00D4;
     int _0x00D8;
     int _0x00DC;
+    /* 0xE0 */
     int _0x00E0;
     int _0x00E4;
     int _0x00E8;
     int _0x00EC;
+    /* 0xF0 */
     int _0x00F0;
     int _0x00F4;
     int _0x00F8;
     int _0x00FC;
+    /* 0x100 */
     int _0x0100;
     int _0x0104;
     int _0x0108;
     int _0x010C;
+    /* 0x110 */
     int _0x0110;
     int _0x0114;
     int _0x0118;
     int _0x011C;
+    /* 0x120 */
     
     /* Previous actor in list */
     struct z64_actor_t * previous;
@@ -106,60 +134,76 @@ struct z64_actor_t
     /* Next actor in list */
     struct z64_actor_t * next;
     
-    u32 v0128;
-    u32 v012C;
-    u32 v0130;
-    u32 v0134;
-    u32 v0138;
+    u32 actor_init;
+    u32 actor_rotuine_1;
+    /* 0x130 */
+    u32 actor_routine_2;
+    u32 actor_routine_3;
+    u32 actor_code_entry;
     u32 v013C;
+    /* 0x140 */
     u32 v0140;
     u32 v0144;
     u32 v0148;
     u32 v014C;
+    /* 0x150 */
     u32 v0150;
     u32 v0154;
     u32 v0158;
     u32 v015C;
+    /* 0x160 */
     u32 v0160;
     u32 v0164;
-    u32 v0168;
+    f32 v0168;
     u32 v016C;
+    /* 0x170 */
     u32 v0170;
     u32 v0174;
     u32 v0178;
     u32 v017C;
+    /* 0x180 */
     u32 v0180;
     u32 v0184;
     u32 v0188;
     u32 v018C;
-    u32 v0190;
+    /* 0x190 */
+    u32 next_routine;
     u32 v0194;
     u32 v0198;
-    u32 v019C;
+    u16 v019C;
+    u16 v019E;
+    /* 0x1A0 */
     u32 v01A0;
     u32 v01A4;
-    u32 v01A8;
-    u32 v01AC;
-    u32 v01B0;
-    u32 v01B4;
-    u32 v01B8;
-    u32 v01BC;
-    u32 v01C0;
+    f32 v01A8;
+    f32 v01AC;
+    /* 0x1B0 */
+    f32 v01B0;
+    f32 v01B4;
+    f32 v01B8;
+    f32 v01BC;
+    /* 0x1C0 */
+    u16 v01C0;
+    u16 v01C2
     u32 v01C4;
     u32 v01C8;
     u32 v01CC;
+    /* 0x1D0 */
     u32 v01D0;
     u32 v01D4;
     u32 v01D8;
     u32 v01DC;
+    /* 0x1E0 */
     u32 v01E0;
     u32 v01E4;
     u32 v01E8;
     u32 v01EC;
+    /* 0x1F0 */
     u32 v01F0;
     u32 v01F4;
     u32 v01F8;
     u32 v01FC;
+    /* 0x200 */
 };
 
 
