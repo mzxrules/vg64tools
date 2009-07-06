b68d1c5f60764aad9ca70d5c84c81e5f294b368a
zzt256 <zzt256@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-06 17:25:42 +0000

    En_Bird: func8 replaced & verified working

--- n64/z64-actors/En_Bird/func.c
+++ n64/z64-actors/En_Bird/func.c
@@ -51,3 +51,17 @@ void En_Bird_f2 ( void * _a0, void * _a1 )
         (void*)(int)a0->variable 
     );
 }
+
+
+void En_Bird_f8 ( void * a0, void * a1 )
+{
+    struct z64_actor_t * a;
+    
+    a = (struct z64_actor_t*)a0;
+    
+    AVAL(a0,f32,0x1B8) += AVAL(a0,f32,0x1BC);
+    
+    /* AVAL(a0,ZAFunc,400)( a0, a1); */
+    a->f_next( a0, a1 );
+}
+
