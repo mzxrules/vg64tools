720181bb0e248f1b04b931e7807b2da998b2ecf2
zzt256 <zzt256@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-06 08:57:54 +0000

    Framework for recreating of En_Bird

---
+++ n64/z64-actors/En_Bird/bird.h
@@ -0,0 +1,15 @@
+#ifndef __Z_EN_BIRD_H__
+#define __Z_EN_BIRD_H__
+
+extern void En_Bird_f1 ( void *, void * );
+extern void En_Bird_f2 ( void *, void * );
+extern void En_Bird_f3 ( void *, void * );
+extern void En_Bird_f4 ( void *, void * );
+extern void En_Bird_f5 ( void *, void * );
+extern void En_Bird_f6 ( void *, void * );
+extern void En_Bird_f7 ( void *, void * );
+extern void En_Bird_f8 ( void *, void * );
+extern void En_Bird_f9 ( void *, void * );
+
+#endif /* __Z_EN_BIRD_H__ */
+
