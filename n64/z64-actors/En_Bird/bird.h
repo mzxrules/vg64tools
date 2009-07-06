1710e528a566aeae536fc66df2416ca04328cce5
zzt256 <zzt256@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-06 16:37:00 +0000

    En_Bird: func2 replaced & verified working

--- n64/z64-actors/En_Bird/bird.h
+++ n64/z64-actors/En_Bird/bird.h
@@ -11,5 +11,21 @@ extern void En_Bird_f7 ( void *, void * );
 extern void En_Bird_f8 ( void *, void * );
 extern void En_Bird_f9 ( void *, void * );
 
+extern u32 En_Bird_dat1[];
+
+extern void func_800780DC( void *, u32 * );
+extern void func_8002D62C( struct z64_actor_old_t *, u32 );
+extern void func_800A457C( u32*, struct z64_actor_old_t *, u32 hierarchy, u32 animation, u32, u32, u32 );
+extern void func_8002B1E0( struct z64_actor_old_t *, u32, u32, u32 );
+extern u32  func_800A2000( u32 animation );
+extern void func_80077D90( int, int );
+extern void func_800A51A0( struct z64_actor_old_t *, u32 animation, u32, f32, u32, u32, f32 );
+extern f32  func_80100290( f32 );
+extern void func_80078310( struct z64_actor_old_t *, f32, u32, f32, f32 );
+extern void func_800A49FC( u32 );
+extern u32  func_80078068( u32, u32);
+extern void func_80077B58( u32, u32, u16 );
+extern void func_800A15C8( void *, u32, u32, u32 );
+
 #endif /* __Z_EN_BIRD_H__ */
 
