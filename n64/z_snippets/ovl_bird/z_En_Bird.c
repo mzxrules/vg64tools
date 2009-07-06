bcbb2121e6a93e11c1f4efaf1e8bb3ceee1c07c6
spinout_182@yahoo.com <spinout_182@yahoo.com@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-06 09:03:49 +0000

    The bird compiles now, but with lots of  warnings.

--- n64/z_snippets/ovl_bird/z_En_Bird.c
+++ n64/z_snippets/ovl_bird/z_En_Bird.c
@@ -3,29 +3,29 @@
 */
 
 #include "../actor.h"
-#include "mips.h"
+#include "../mips.h"
 
 /* Local functions */
-extern void bird_func0x809C1BB0( struct z64_actor_t * a0, u32 * a1 );
-extern void bird_func0x809C1BB8( struct z64_actor_t * a0, u32 * a1 );
+extern void bird_func0x809C1BB0( struct z64_actor_t * a0, ZAFunc * a1 );
+extern void bird_func0x809C1BB8( struct z64_actor_old_t * a0, u32 * a1 );
 extern void bird_func0x809C1CA0( u32 * a0, u32 * a1);
-extern void bird_func0x809C1CAC( struct z64_actor_t * a0, u32 * a1 );
-extern void bird_func0x809C1D60( struct z64_actor_t * a0, u32 * a1 );
+extern void bird_func0x809C1CAC( struct z64_actor_old_t * a0, u16 a1 );
+extern void bird_func0x809C1D60( struct z64_actor_old_t * a0, u32 * a1 );
 extern void bird_func0x809C1E00( void * a0, u32 * actor_var);
-extern void bird_func0x809C1E40( struct z64_actor_t * a0, u32 * a1 );
+extern void bird_func0x809C1E40( struct z64_actor_old_t * a0, u32 * a1 );
 extern void bird_func0x809C1F5C( struct z64_actor_t * a0, u32 * a1 );
-extern void bird_func0x809C1F8C( struct z64_actor_t * a, void * wut );
+extern void bird_func0x809C1F8C( struct z64_actor_old_t * a, void * wut );
 
 /* External functions */
 extern void func_800780DC( u32, u32 location );
-extern void func_8002D62C( u32, u32 );
-extern void func_800A457C( u32, u32, u32 hierarchy, u32 animation, u32, u32, u32 );
-extern void func_8002B1E0( u32, u32, u32 * a2 );
+extern void func_8002D62C( struct z64_actor_old_t *, u32 );
+extern void func_800A457C( u32*, struct z64_actor_old_t *, u32 hierarchy, u32 animation, u32, u32, u32 );
+extern void func_8002B1E0( struct z64_actor_old_t *, u32, u32 a2 );
 extern u32  func_800A2000( u32 animation );
 extern void func_80077D90( int, int );
-extern void func_800A51A0( u32, u32 animation, u32, f32, u32, u32, f32 );
+extern void func_800A51A0( struct z64_actor_old_t *, u32 animation, u32, f32, u32, u32, f32 );
 extern f32  func_80100290( f32 );
-extern void func_80078310( u32, f32, u32, f32, f32 );
+extern void func_80078310( struct z64_actor_old_t *, f32, u32, f32, f32 );
 extern void func_800A49FC( u32 );
 extern u32  func_80078068( u32, u32);
 extern void func_80077B58( u32, u32, u16 );
@@ -33,6 +33,7 @@ extern void func_800A15C8( void *, u32, u32, u32 );
 
 MK_AHEAD
 (
+    spec,
     0x72,	/* Actor number */
     0x6,	/* Actor type */
     0,		/* Actor status */
@@ -46,17 +47,17 @@ MK_AHEAD
 );
     
 
-void bird_func0x809C1BB0(struct z64_actor_t * a0, ZAFunc * a1)
+void bird_func0x809C1BB0( struct z64_actor_t * a0, ZAFunc * a1 )
 {
-    AVAL(a0,ZAFunc,0x190) = a1;
+    AVAL(a0,ZAFunc,0x190) = (ZAFunc)a1;
 }
 
-void bird_func0x809C1BB8(struct z64_actor_old_t * a0, u32 * a1)
+void bird_func0x809C1BB8( struct z64_actor_old_t * a0, u32 * a1 )
 {
     func_800780DC(0, 0x809C1FF0);
     func_8002D62C(a0, 0x3C23D70A);
     func_800A457C(a1, a0 + 0x014C, 0x06002190, 0x0600006C, 0, 0, 0); /* Animation init */
-    func_8002B1E0(a0 + 0x00B4, 0x45ABE000, 0x8003B5EC);
+    func_8002B1E0(a0 + 0x00B4, 0x45ABE000, (u32)0x8003B5EC);
       
     a0->_0x00AE = 0;
     a0->v0194 = 0;
@@ -73,7 +74,7 @@ void bird_func0x809C1BB8(struct z64_actor_old_t * a0, u32 * a1)
     bird_func0x809C1CAC(a0, a0->variable); /* Local */
 }
 
-void bird_func0x809C1CA0(u32 * a0, u32 * a1)
+void bird_func0x809C1CA0( u32 * a0, u32 * a1 )
 {
     return;
 /* wtf?
@@ -83,7 +84,7 @@ sw	a1,4(sp)
 */
 }
 
-void bird_func0x809C1CAC(struct z64_actor_old_t * a0, u32 * a1)
+void bird_func0x809C1CAC( struct z64_actor_old_t * a0, u16 a1 )
 {
     u32 v0;
     f32 f0;
@@ -102,7 +103,7 @@ void bird_func0x809C1CAC(struct z64_actor_old_t * a0, u32 * a1)
 
     func_80077D90(5, 35);
     func_800A51A0(a0 + 0x14C, 0x0600006C, f0, 0.0f, v0, 0, 0.0f);
-    bird_func0x809C1BB0(a0, 0x809C1D60); /* Local */
+    bird_func0x809C1BB0(a0, (ZAFunc)0x809C1D60); /* Local */
 }
 
 void bird_func0x809C1D60(struct z64_actor_old_t * a0, u32 * a1)
@@ -111,7 +112,7 @@ void bird_func0x809C1D60(struct z64_actor_old_t * a0, u32 * a1)
     
     f0 = func_80100290(a0->v01B4);
     func_80078310(a0 + 0x68, 0.0, 0x3DCCCCCD, 0.5, 0.0);
-    a0->_0x00BC = a0->_0x00BC+(a0->_0x01A0 f0);
+    a0->_0x00BC = a0->_0x00BC+(a0->v01A0 * f0);
 
     if (a0->v019C == 0)
     {
@@ -144,7 +145,7 @@ void bird_func0x809C1E40(struct z64_actor_old_t * a0, u32 * a1)
     func_80078310(a0+0x68, a0->v01A8, 0x3DCCCCCD, a0->v01AC, 0);
     f0 =_func0x80078028(a0+0x24, a0+0x2C);
     
-    if !(a0->v01B0 < f0) && !(a0->v0198 < 4)
+    if ((!(a0->v01B0 < f0)) && (!(a0->v0198 < 4)))
     {
         f0 = func_80100290(a0->v01B4);
         tmp = f0 * a0->v01A4;
