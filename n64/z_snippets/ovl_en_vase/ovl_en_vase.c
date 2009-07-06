1bbc491e6d29d39193361c467577ece44cae31db
zzt256 <zzt256@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-06 06:30:49 +0000

    Vase actor updates to reflect actor.h changes.

--- n64/z_snippets/ovl_en_vase/ovl_en_vase.c
+++ n64/z_snippets/ovl_en_vase/ovl_en_vase.c
@@ -10,38 +10,34 @@ extern char obj_8002B5EC;
 extern void vase_init ( struct z64_actor_t * a, void * o );
 extern void vase_null ( struct z64_actor_t * a, void * o );
 extern void vase_last ( struct z64_actor_t * a, void * o );
-struct head_t
-{
-    u32 spec[4];
-    void * spec2[4];
-};
-struct head_t spec =
-{
-    {
-        0x00820600,
-        0x00000010,
-        0x00860000,
-        0x0000014C
-    },
-    {
-        vase_init,
-        vase_null,
-        (void*)0x80035118,
-        vase_last
-    }
-};
+
+
+/* Create actor header */
+MK_AHEAD
+( 
+    spec, 
+    0x82, 
+    6, 
+    0,
+    0x10,
+    0x86,
+    0x14C,
     
+    vase_init, 
+    vase_null, 
+    (void*)0x80035118, 
+    vase_last 
+);
+
 
 void
 vase_init ( struct z64_actor_t * a, void * o )
 {
     func_8002d62c( a, 0x3c23d70a );
     
-    *(int*)((u8*)a + 56) = *(int*)((u8*)a + 36);
-    *(int*)((u8*)a + 60) = *(int*)((u8*)a + 40);
-    *(int*)((u8*)a + 64) = *(int*)((u8*)a + 44);
+    a->coords_3 = a->coords_2;
     
-    func_8002b1e0( &a->_0x00B4, 0, &obj_8002B5EC, 0x40c00000 );
+    func_8002b1e0( AADDR(a, 0x00B4), 0, &obj_8002B5EC, 0x40c00000 );
 }
 
 void
