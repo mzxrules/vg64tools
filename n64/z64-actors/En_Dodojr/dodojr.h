4432f34d5dac25f227b7d3fe663d18c589208fe0
spinout_182@yahoo.com <spinout_182@yahoo.com@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-08 21:27:00 +0000

    En_Dodojr: A more complex, full overlay. Original ASM is still being reformatted.

---
+++ n64/z64-actors/En_Dodojr/dodojr.h
@@ -0,0 +1,86 @@
+#ifndef __Z_EN_DODOJR_H__
+#define __Z_EN_DODOJR_H__
+
+#include "mips.h"
+#include "actor.h"
+
+extern void En_Dodojr_f1 ( void *, void * );	/* 809f63c0 */
+extern void En_Dodojr_f2 ( void *, void * );	/* 809f64a8 */
+extern void En_Dodojr_f3 ( void *, void * );	/* 809f64d0 */
+extern void En_Dodojr_f4 ( void *, void * );	/* 809f6510 */
+extern void En_Dodojr_f5 ( void *, void * );	/* 809f6730 */
+extern void En_Dodojr_f6 ( void *, void * );	/* 809f68b0 */
+extern void En_Dodojr_f7 ( void *, void * );	/* 809f6994 */
+extern void En_Dodojr_f8 ( void *, void * );	/* 809f6a20 */
+extern void En_Dodojr_f9 ( void *, void * );	/* 809f6ac4 */
+extern void En_Dodojr_f10 ( void *, void * );	/* 809f6b38 */
+extern void En_Dodojr_f11 ( void *, void * );	/* 809f6bbc */
+extern void En_Dodojr_f12 ( void *, void * );	/* 809f6c24 */
+extern void En_Dodojr_f13 ( void *, void * );	/* 809f6ca4 */
+extern void En_Dodojr_f14 ( void *, void * );	/* 809f6dd0 */
+extern void En_Dodojr_f15 ( void *, void * );	/* 809f6e54 */
+extern void En_Dodojr_f16 ( void *, void * );	/* 809f706c */
+extern void En_Dodojr_f17 ( void *, void * );	/* 809f7094 */
+extern void En_Dodojr_f18 ( void *, void * );	/* 809f709c */
+extern void En_Dodojr_f19 ( void *, void * );	/* 809f70e8 */
+extern void En_Dodojr_f20 ( void *, void * );	/* 809f72a4 */
+extern void En_Dodojr_f21 ( void *, void * );	/* 809f73ac */
+extern void En_Dodojr_f22 ( void *, void * );	/* 809f74c4 */
+extern void En_Dodojr_f23 ( void *, void * );	/* 809f758c */
+extern void En_Dodojr_f24 ( void *, void * );	/* 809f768c */
+extern void En_Dodojr_f25 ( void *, void * );	/* 809f773c */
+extern void En_Dodojr_f26 ( void *, void * );	/* 809f77ac */
+extern void En_Dodojr_f27 ( void *, void * );	/* 809f784c */
+extern void En_Dodojr_f28 ( void *, void * );	/* 809f786c */
+extern void En_Dodojr_f29 ( void *, void * );	/* 809f78ec */
+extern void En_Dodojr_f30 ( void *, void * );	/* 809f799c */
+extern void En_Dodojr_f31 ( void *, void * );	/* 809f7a00 */
+extern void En_Dodojr_f32 ( void *, void * );	/* 809f7ab8 */
+extern void En_Dodojr_f33 ( void *, void * );	/* 809f7b3c */
+extern void En_Dodojr_f34 ( void *, void * );	/* 809f7be4 */
+extern void En_Dodojr_f35 ( void *, void * );	/* 809f7c48 */
+extern void En_Dodojr_f36 ( void *, void * );	/* 809f7c90 */
+extern void En_Dodojr_f37 ( void *, void * );	/* 809f7d50 */
+extern void En_Dodojr_f38 ( void *, void * );	/* 809f7dfc */
+extern void En_Dodojr_f39 ( void *, void * );	/* 809f7e10 */
+
+extern void func_8002B1E0 (  );
+extern void func_800A457C (  );
+extern void func_8005C364 (  );
+extern void func_8005C4AC (  );
+extern void func_8005B200 (  );
+extern void func_80061EFC (  );
+extern void func_8002D62C (  );
+extern void func_8005C3AC (  );
+extern void func_8002F828 (  );
+extern void func_8003426C (  );
+extern void func_800FD9AC (  );
+extern void func_80077870 (  );
+extern void func_80077834 (  );
+extern void func_8002836C (  );
+extern void func_800A2000 (  );
+extern void func_800A51A0 (  );
+extern void func_80077F90 (  );
+extern void func_80100290 (  );
+extern void func_80104610 (  );
+extern void func_80078068 (  );
+extern void func_8007869C (  );
+extern void func_80032C7C (  );
+extern void func_800626DC (  );
+extern void func_8005D79C (  );
+extern void func_8005D9F4 (  );
+extern void func_8005DC4C (  );
+extern void func_8002D868 (  );
+extern void func_8002D570 (  );
+extern void func_80031F50 (  );
+extern void func_8001F940 (  );
+extern void func_800A49FC (  );
+extern void func_8002D8E0 (  );
+extern void func_8002E4B4 (  );
+extern void func_8002D5B8 (  );
+extern void func_800D0A8C (  );
+extern void func_800D1AF4 (  );
+extern void func_80093D18 (  );
+extern void func_800A15C8 (  );
+
+#endif /* __Z_EN_DODOJR_H__ */
