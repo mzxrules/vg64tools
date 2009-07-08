674311f7c09c61fb0d20b22f75f66bc5355134a9
spinout_182@yahoo.com <spinout_182@yahoo.com@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-08 09:03:08 +0000

    En_Bird: Integration of f7, and floats passsed as u32s have been changed to floats. Animation plays too fast, but that's fixable.

--- n64/z64-actors/En_Bird/bfunc-2.c
+++ n64/z64-actors/En_Bird/bfunc-2.c
@@ -11,7 +11,7 @@ void En_Bird_f2 ( void * _a0, void * _a1 )
     
     func_800780DC( a0, En_Bird_dat1 );
     
-    func_8002D62C( a0, 0x3C23D70A );
+    func_8002D62C( a0, 0.01f );
     
     /* Animation init */
     func_800A457C(
@@ -36,12 +36,12 @@ void En_Bird_f2 ( void * _a0, void * _a1 )
     AVAL(a0,u32,408) = 0;
     AVAL(a0,u32,416) = 0;
     AVAL(a0,u32,420) = 0;
-    AVAL(a0,float,424) = 0.5f; /* Animation speed */
-    AVAL(a0,u32,428) = 0x3F000000;
-    AVAL(a0,u32,432) = 0x42200000;
+    AVAL(a0,f32,424) = 1.5f; /* Animation speed */
+    AVAL(a0,f32,428) = 0.5f;
+    AVAL(a0,f32,432) = 40.0f;
     
     AVAL(a0,u32,440) = 0;
-    AVAL(a0,u32,444) = 0x428C0000;
+    AVAL(a0,u32,444) = 70.0f;
     
     AVAL(a0,u16,448) = 0x09C4;
     
