b88c467360707722ebe196c25b137ee09ca5e873
spinout_182@yahoo.com <spinout_182@yahoo.com@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-09 19:04:15 +0000

    En_Dodojr: Function 3 replaced and working.

--- n64/z64-actors/En_Dodojr/trunk/c/dfunc-03.c
+++ n64/z64-actors/En_Dodojr/trunk/c/dfunc-03.c
@@ -1,7 +1,18 @@
 #include "../dodojr.h"
 
-/*
-void En_Bird_f3 (void * a0, void * a1 )
+
+void En_Dodojr_f3 (void * a0, void * a1 )
 {
+    func_8002F828 (
+	a0,
+	6158
+    );
+    func_8003426C (
+	a0,
+	16384,
+	200,
+	0,
+	8
+    );
 }
-*/
+
