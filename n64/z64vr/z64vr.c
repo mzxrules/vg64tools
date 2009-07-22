dd715735ae82993e2b1b560604af66e40a8b80e6
spinout_182@yahoo.com <spinout_182@yahoo.com@8c0d48dc-6448-11de-89e8-69fd2269e136>
2009-07-22 22:01:38 +0000

    commented main source file

--- n64/z64vr/z64vr.c
+++ n64/z64vr/z64vr.c
@@ -1,23 +1,27 @@
+/*
+ Feel free to do as you like with this, so long as I (spinout) am credited as the original author.
+*/
+
 #define TEXT_OFF 0x80221EAA
 
-void _start(void)
+void _start(void)	/* What makes this hack work is that it runs (almost) right after the text is DMA'd from ROM*/
 {
-    char (*text)[0xC0] = (void*)TEXT_OFF;
-    int i = 0;
-    while (1)
+    char (*text)[0xC0] = (void*)TEXT_OFF;	/* Text fresh from ROM */
+    int i = 0;	/* Counter */
+    while (1)	/* mips-gcc messes up for loops */
     {
-        if ( (*text)[i-1] > 0x1F )
+        if ( (*text)[i-1] > 0x1F )	/* Don't edit a special text command! */
         {
-            if ( ((*text)[i] == 97) || ((*text)[i] == 101) || ((*text)[i] == 105) || ((*text)[i] == 117) )
+            if ( ((*text)[i] == 97) || ((*text)[i] == 101) || ((*text)[i] == 105) || ((*text)[i] == 117) ) /* Lowercase */
             {
                 (*text)[i] = 111;
             }
-            else if ( ((*text)[i] == 65) || ((*text)[i] == 69) || ((*text)[i] == 73) || ((*text)[i] == 85) )
+            else if ( ((*text)[i] == 65) || ((*text)[i] == 69) || ((*text)[i] == 73) || ((*text)[i] == 85) ) /* Uppercase */
             {
                 (*text)[i] = 79;
             }
         }
-        else if ( ( (*text)[i] == 2 ) || ( i == 0xC0 ) )
+        else if ( ( (*text)[i] == 2 ) || ( i == 0xC0 ) )	/* \x02 = EOF, 0xC0 = max len*/
         {
             break;
         }
